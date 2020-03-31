f4d81136d53f097b48250c107013bf49d0c0c6b5
Kiran Paul VJ <KiranPaul.VJ@cognizant.com>
2020-03-31 20:05:28 +0530

    Mar-31 status update

--- src/data/stats.js
+++ src/data/stats.js
@@ -12,8 +12,7 @@ Idukki, and Wayanad ",126,6,6,0,0,5,0,676,42517
 29-03-2020,141211,140618,593,6690,5518,202,20,,181,15,0,1,0,0,0,849,70822
 30-03-2020,157283,156660,623,6991,6034,234,32,"Out of this 17 cases are from
 Kasaragod, 11 from Kannur and 2 each from Wayanad and Idukki.",213,17,2,1,0,0,0,849,77076
-31-03-2020,157283,156660,623,6991,6034,234,32,"Out of this 17 cases are from
-Kasaragod, 11 from Kannur and 2 each from Wayanad and Idukki.",213,17,2,2,0,0,0,849,77076
+31-03-2020,163129,162471,658,7485,6381,241,7,"Out of this 2 cases each from Thiruvananthapuram ,Kasaragod, and 1 each from Kollam,Kannur, and Thrissur",215,17,0,2,1,0,0,849,84166
 `;
 
-export const LAST_UPDATED = "28-March-2020";
+export const LAST_UPDATED = "31-March-2020";
